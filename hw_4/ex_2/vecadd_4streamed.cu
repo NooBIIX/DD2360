dcb8330214f7ce1ee30107924f33ee8b242a92f1
NooBIIX <119602903+NooBIIX@users.noreply.github.com>
2023-01-17 02:21:44 +0100

    Add files via upload

---
+++ hw_4/ex_2/vecadd_4streamed.cu
@@ -0,0 +1,138 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+#define DataType double
+#define TPB 1024
+
+__global__ void vecAdd(DataType *in1, DataType *in2, DataType *out, int len) {
+  //@@ Insert code to implement vector addition here
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if(id < len) out[id] = in1[id] + in2[id];
+}
+
+//@@ Insert code to implement timer start
+double timestart(){
+  struct timeval t_start;
+  gettimeofday(&t_start, NULL);
+  return (double) (1000000.0*(t_start.tv_sec) + t_start.tv_usec)/1000000.0;
+}
+//@@ Insert code to implement timer stop
+double timestop(){
+  struct timeval t_stop;
+  gettimeofday(&t_stop, NULL);
+  return (double) (1000000.0*(t_stop.tv_sec) + t_stop.tv_usec)/1000000.0;
+}
+
+int main(int argc, char **argv) {
+  
+  int inputLength;
+  DataType *hostInput1;
+  DataType *hostInput2;
+  DataType *hostOutput;
+  DataType *resultRef;
+  DataType *deviceInput1;
+  DataType *deviceInput2;
+  DataType *deviceOutput;
+
+  //@@ Insert code below to read in inputLength from args
+
+  if(argc > 1) inputLength = atoi(argv[1]);
+
+  printf("The input length is %d\n", inputLength);
+  
+  //@@ Insert code below to allocate Host memory for input and output
+  size_t bytes = inputLength * sizeof(double);
+
+
+  hostInput1 = (double *)malloc(bytes);
+  hostInput2 = (double *)malloc(bytes);
+  hostOutput = (double *)malloc(bytes);
+  resultRef = (double *)malloc(bytes);
+
+
+  //@@ Insert code below to initialize hostInput1 and hostInput2 to random numbers, and create reference result in CPU
+  srand((unsigned)time(NULL));
+  for(int i = 0; i < inputLength - 1; i++){
+    hostInput1[i] = ((double)rand()/(double)RAND_MAX);
+    hostInput2[i] = ((double)rand()/(double)RAND_MAX);
+    resultRef[i] = hostInput1[i] + hostInput2[i];
+  }
+
+  //@@ Insert code below to allocate GPU memory here
+  cudaMalloc(&deviceInput1, bytes);
+  cudaMalloc(&deviceInput2, bytes);
+  cudaMalloc(&deviceOutput, bytes);
+
+
+  //@@ Insert code to below to Copy memory to the GPU here
+
+  int num_str = 4;
+  int S_seg=(inputLength+3)/4; 
+
+  cudaStream_t stream[num_str];
+
+  for(int i=0;i<num_str;i++){
+    cudaStreamCreate(&stream[i]);
+  }
+
+  double start1 = timestart();
+
+  for(int i=0;i<num_str;i++){
+    cudaMemcpyAsync(deviceInput1+i*S_seg, hostInput1+i*S_seg, S_seg * sizeof(DataType), cudaMemcpyHostToDevice, stream[i]);
+    cudaMemcpyAsync(deviceInput2+i*S_seg, hostInput2+i*S_seg, S_seg * sizeof(DataType), cudaMemcpyHostToDevice, stream[i]);
+  }
+  double t1me = timestop() - start1;
+  printf("Transfer time host to device %f seconds\n", t1me);
+
+
+  //@@ Initialize the 1D grid and block dimensions here
+  int blockSize, gridSize;
+
+  blockSize = TPB;
+  gridSize = (inputLength+blockSize-1) / blockSize;
+  //printf("gridSize is %d\n", gridSize);
+
+  //@@ Launch the GPU Kernel here
+  double start_kernel = timestart();
+  for(int i=0;i<num_str;i++){ 
+    vecAdd<<<gridSize, blockSize, 0, stream[i]>>>(deviceInput1+i*S_seg, deviceInput2+i*S_seg, deviceOutput+i*S_seg, S_seg);
+  }
+  
+  //cudaDeviceSynchronize();
+
+  double time_kernel = timestop() - start_kernel;
+  printf("Kernel time %f seconds\n", time_kernel);
+
+  //@@ Copy the GPU memory back to the CPU here
+  double start2 = timestart();
+
+  for(int i=0;i<num_str;i++){
+    cudaMemcpyAsync(hostOutput+i*S_seg, deviceOutput+i*S_seg, S_seg * sizeof(DataType), cudaMemcpyDeviceToHost, stream[i]);
+  }
+
+  double t2me = timestop() - start2;
+  printf("Transfer time device to host %f seconds\n", t2me);
+
+
+  for(int i = 0; i<num_str;i++) {
+      cudaStreamDestroy(stream[i]);
+  }
+  cudaDeviceSynchronize();
+  //@@ Insert code below to compare the output with the reference
+  if (hostOutput == resultRef) printf("The output is same as the reference!\n");
+
+  //@@ Free the GPU memory here
+  cudaFree(deviceInput1);
+  cudaFree(deviceInput2);
+  cudaFree(deviceOutput);
+
+  //@@ Free the CPU memory here
+  free(hostInput1);
+  free(hostInput2);
+  free(hostOutput);
+  free(resultRef);
+
+  return 0;
+}
---
+++ hw_4/ex_3/vecmul_pinned.cu
@@ -0,0 +1,151 @@
+
+#include <stdio.h>
+#include <sys/time.h>
+
+#define DataType double
+#define TPB 32
+
+// Compute C = A * B
+__global__ void gemm(DataType *A, DataType *B, DataType *C, int numARows,
+                      int numAColumns, int numBRows, int numBColumns){
+  //@@ Insert code to implement matrix multiplication here
+  int col_id = blockIdx.x * blockDim.x + threadIdx.x;
+  int row_id = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if ((col_id<numBColumns)&&(row_id<numARows)){
+    DataType tmp = 0;
+    for (int i=0;i<numAColumns;i++){
+      tmp += A[numAColumns*row_id+i] * B[numBColumns*i+col_id];
+    }
+    C[numBColumns*row_id+col_id] = tmp;
+  }
+}
+
+//@@ Insert code to implement timer start
+double timestart(){
+  struct timeval t_start;
+  gettimeofday(&t_start, NULL);
+  return (double) (1000000.0*(t_start.tv_sec) + t_start.tv_usec)/1000000.0;
+}
+//@@ Insert code to implement timer stop
+double timestop(){
+  struct timeval t_stop;
+  gettimeofday(&t_stop, NULL);
+  return (double) (1000000.0*(t_stop.tv_sec) + t_stop.tv_usec)/1000000.0;
+}
+
+int main(int argc, char **argv) {
+  
+  DataType *hostA; // The A matrix
+  DataType *hostB; // The B matrix
+  DataType *hostC; // The output C matrix
+  DataType *resultRef; // The reference result
+  DataType *deviceA;
+  DataType *deviceB;
+  DataType *deviceC;
+  int numARows;    // number of rows in the matrix A
+  int numAColumns; // number of columns in the matrix A
+  int numBRows;    // number of rows in the matrix B
+  int numBColumns; // number of columns in the matrix B
+  int numCRows;
+  int numCColumns;
+
+  //@@ Insert code below to read in numARows, numAColumns, numBColumns from args
+  
+  numARows = atoi(argv[1]);
+  numAColumns = atoi(argv[2]);
+  numBRows = atoi(argv[3]);
+  numBColumns = atoi(argv[4]);
+  numCRows = numARows;
+  numCColumns = numBColumns;
+
+  printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
+  
+  //@@ Insert code below to allocate Host memory for input and output
+
+  double start0 = timestart();
+
+  cudaHostAlloc(&hostA, numARows * numAColumns * sizeof(DataType), cudaHostAllocDefault);
+  cudaHostAlloc(&hostB, numBRows * numBColumns * sizeof(DataType), cudaHostAllocDefault);
+  cudaHostAlloc(&hostC, numCRows * numCColumns * sizeof(DataType), cudaHostAllocDefault);
+  cudaHostAlloc(&resultRef, numCRows * numCColumns * sizeof(DataType), cudaHostAllocDefault);
+
+  double time0 = timestop() - start0;
+  printf("Pinned memory allocation %f seconds.\n", time0);
+  //resultRef = (DataType*) malloc(numCRows * numCColumns * sizeof(DataType));
+  
+  //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+  srand((unsigned)time(NULL));
+  for(int i=0;i<numARows;i++){
+    for (int j=0;j<numAColumns;j++){
+      hostA[i*numAColumns+j] = (DataType)rand() / (DataType)RAND_MAX;
+    }
+  }
+
+    for(int i=0;i<numBRows;i++){
+    for (int j=0;j<numBColumns;j++){
+      hostB[i*numBColumns+j] = (DataType)rand() / (DataType)RAND_MAX;
+    }
+  }
+
+  for(int i=0;i<numARows;i++) {
+        for(int j=0;j<numBColumns;j++) {
+          resultRef[i*numBColumns+j] = 0;
+          for(int k=0;k<numAColumns;k++) {
+            resultRef[i*numBColumns+j] += hostA[i*numAColumns+k] * hostB[k*numBColumns+j];
+          }        
+        }
+  }
+
+  //@@ Insert code below to allocate GPU memory here
+
+  cudaMalloc(&deviceA, numARows * numAColumns * sizeof(DataType));
+  cudaMalloc(&deviceB, numBRows * numBColumns * sizeof(DataType));
+  cudaMalloc(&deviceC, numCRows * numCColumns * sizeof(DataType));
+
+  //@@ Insert code to below to Copy memory to the GPU here
+
+  double start1 = timestart();
+  cudaMemcpy(deviceA, hostA, numARows * numAColumns * sizeof(DataType), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceB, hostB, numBRows * numBColumns * sizeof(DataType), cudaMemcpyHostToDevice);
+  double time1 = timestop() - start1;
+  printf("Transfer time host to device %f seconds\n", time1);
+
+  //@@ Initialize the grid and block dimensions here
+
+  int dimBlockx= TPB;
+  int dimBlocky= TPB;
+
+  int dimGridx= (numCColumns+dimBlockx-1)/dimBlockx;
+  int dimGridy= (numCRows+dimBlocky-1)/dimBlocky;
+
+  //@@ Launch the GPU Kernel here
+  double start2 = timestart();
+  gemm<<<dim3(dimGridx,dimGridy,1),dim3(dimBlockx,dimBlocky,1)>>>(deviceA,deviceB,deviceC,numARows,numAColumns,numBRows,numBColumns);
+  cudaDeviceSynchronize();
+  double time2 = timestop() - start2;
+  printf("Kernel Time %f seconds\n", time2);
+
+  //@@ Copy the GPU memory back to the CPU here
+  
+  double start3=timestart();
+  cudaMemcpy(hostC, deviceC, numCRows * numCColumns * sizeof(DataType), cudaMemcpyDeviceToHost);
+  double time3 = timestop() - start3;
+  printf("Transfer Time device to host %f seconds\n", time3);
+
+  //@@ Insert code below to compare the output with the reference
+ if (hostC == resultRef) printf("The output is same as the reference!\n");
+
+  //@@ Free the GPU memory here
+  cudaFree(deviceA);
+  cudaFree(deviceB);
+  cudaFree(deviceC);
+
+  //@@ Free the CPU memory here
+  cudaFreeHost(hostA);
+  cudaFreeHost(hostB);
+  cudaFreeHost(hostC);
+  cudaFreeHost(resultRef);
+
+  return 0;
+}
